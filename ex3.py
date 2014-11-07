c521f9f4d3ff2b03958b49c47d6068a899367912
John Scancella <jsca@loc.gov>
2014-11-07 09:42:47 -0500

    adding exercise 3

--- ex3.py
+++ ex3.py
@@ -1,14 +1,18 @@
+#prompt for user
 print "I will now count my chickens:"
 
+#example showing printing words followed by a space and then a numeric value
 print "Hens",  25 + 30 / 6
 print "roosters",  100 -25 * 3 % 4
 
 print "Now I will count the eggs:"
 
+# evaluates the math expression and prints the result
 print 3 + 2 + 1 - 5 + 4 % 2 - 1  / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# prints a boolean statement based on the <
 print 3 + 2 < 5 - 7
 
 print "What is 3 + 2?", 3 + 2
@@ -18,6 +22,7 @@ print "Oh, what's why it's False."
 
 print "How about some more."
 
+#example of printing words followed by a space and a boolean
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
