058e832d0419d2a66ca2554723bba00e2c1ac4d1
John Scancella <jsca@loc.gov>
2014-11-07 10:28:46 -0500

    adding exercise  7

---
+++ ex7.py
@@ -0,0 +1,23 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 # print out the . charactor 10 times
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#by adding a comma at the end it concatenates the two prints together with a space. so we get Cheese Burger instead of 
+#cheese
+#burger
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
