241dc3c2111322608fdd65527f41b7375cde7948
John Scancella <jsca@loc.gov>
2014-11-07 09:31:00 -0500

    adding second exercise

---
+++ ex2.py
@@ -0,0 +1,7 @@
+# a coment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # a comment after is ignored
+# print "this won't run."
+
+print "this will run." 
